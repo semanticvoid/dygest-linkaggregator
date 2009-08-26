09234681151d7cebd63e85e1bbbce74ecb30b4d5
anand@semanticvoid.com <anand@semanticvoid.com@7081c796-905d-11de-a0a7-0df66920157f>
2009-08-26 05:01:01 +0000

    - tweet class - status feed reader

---
+++ src/linkaggregator/StatusFeedReader.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package linkaggregator;
+
+import com.sun.syndication.feed.synd.SyndEntryImpl;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.SyndFeedInput;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author anand
+ */
+public class StatusFeedReader {
+
+    private SyndFeed getFeed(String url) throws Exception {
+        URL feedUrl = new URL(url);
+        SyndFeedInput input = new SyndFeedInput(false);
+        SyndFeed feed = input.build(new InputStreamReader(feedUrl.openStream()));
+
+        return feed;
+    }
+
+    private String extractUser(String uri) {
+        String user = null;
+        String[] tokens = uri.split("[/]+");
+
+        if(tokens.length > 2) {
+            user = tokens[2];
+        }
+
+        return user;
+    }
+
+    public ArrayList<Tweet> read(String feedURL) {
+        ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+
+        try {
+            SyndFeed feed = getFeed(feedURL);
+            List<SyndEntryImpl> entries = feed.getEntries();
+            
+            for(SyndEntryImpl entry : entries) {
+                String user = extractUser(entry.getUri());
+                Tweet t = new Tweet(entry.getUri(), user, entry.getDescription().getValue().replace(user + ": ", ""), entry.getPublishedDate());
+                tweets.add(t);
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return tweets;
+    }
+
+    public static void main(String[] args) {
+        StatusFeedReader rdr = new StatusFeedReader();
+        rdr.read("http://twitter.com/statuses/user_timeline/874.rss");
+    }
+}
